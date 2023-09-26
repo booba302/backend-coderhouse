f615fd065ab95541f4ab338b4484b5351a0d430b
Franklin Sanchez <booba302@gmail.com>
2023-09-26 19:03:22 -0300

    status codes

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -2,7 +2,7 @@ import * as CartService from "../services/carts.service.js";
 
 export const GETCarts = async (req, res) => {
   const carts = await CartService.getCarts();
-  res.status(200).send(carts);
+  res.status(carts.code).send(carts);
 };
 
 export const GETCartById = async (req, res) => {
@@ -13,43 +13,43 @@ export const GETCartById = async (req, res) => {
 
 export const POSTCart = async (req, res) => {
   const cart = await CartService.addCart();
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const POSTProductToCart = async (req, res) => {
   const { idCart, idProd } = req.params;
   const cart = await CartService.addProductToCart(idCart, idProd);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const PUTCart = async (req, res) => {
   const { idCart } = req.params;
   const { product } = req.body;
   const cart = await CartService.updateCart(idCart, product);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const PUTQuantityInCart = async (req, res) => {
   const { idCart, idProd } = req.params;
   const { quantity } = req.body;
   const cart = await CartService.updateQtyInCart(idCart, idProd, quantity);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const DELETEProductInCart = async (req, res) => {
   const { idCart, idProd } = req.params;
   const cart = await CartService.delProductInCart(idCart, idProd);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const DELETECart = async (req, res) => {
   const { idCart } = req.params;
   const cart = await CartService.delCart(idCart);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
 
 export const DELETEEmptyCart = async (req, res) => {
   const { idCart } = req.params;
   const cart = await CartService.emptyCart(idCart);
-  res.send(cart);
+  res.status(cart.code).send(cart);
 };
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -16,30 +16,30 @@ export const GETProducts = async (req, res) => {
     filter,
     order
   );
-  res.send(products);
+  res.status(products.code).send(products);
 };
 
 export const GETProductsById = async (req, res) => {
   const { id } = req.params;
   const product = await ProductService.getProductById(id);
-  res.send(product);
+  res.status(product.code).send(product);
 };
 
 export const POSTProduct = async (req, res) => {
   const body = req.body;
   const product = await ProductService.addProduct(body);
-  res.send(product);
+  res.status(product.code).send(product);
 };
 
 export const PUTProduct = async (req, res) => {
   const { id } = req.params;
   const body = req.body;
   const product = await ProductService.updateProduct(id, body);
-  res.send(product);
+  res.status(product.code).send(product);
 };
 
 export const DELETEProduct = async (req, res) => {
   const { id } = req.params;
   const product = await ProductService.deleteProduct(id);
-  res.send(product);
+  res.status(product.code).send(product);
 };
